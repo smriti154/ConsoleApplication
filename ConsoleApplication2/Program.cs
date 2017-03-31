b172c3caeb4fb69c66eb3c1d3336a8b5171f3b8f
smriti154 <smriti.154@gmail.com>
2017-03-31 15:22:17 +0530

    change from feature to master

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication2
         {
             Console.WriteLine("wlcome to app1");
             Console.WriteLine("wlcome to app1 from feature1");
-            Console.WriteLine("wlcome to app1 second change from feature1");
+            Console.WriteLine("wlcome to app1 second change from master");
 
 
         }
