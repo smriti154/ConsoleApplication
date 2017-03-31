bb1d04dd4d34014b2685ac96b3c218d11c5d8f76
smriti154 <smriti.154@gmail.com>
2017-03-31 15:47:20 +0530

    commit from faertr1

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication2
         {
             Console.WriteLine("wlcome to app1");
             Console.WriteLine("wlcome to app1 from feature1");
-            Console.WriteLine("master change");
+            Console.WriteLine("feature change");
 
         }
     }
