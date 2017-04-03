409d1165d9816c5995bddcfa469df5e4050240f9
smriti154 <smriti.154@gmail.com>
2017-04-03 12:40:47 +0530

    Revert this  "ocmit"
    This reverts commit 8226d74c8dc6e66ab4e9be2180307fb9b30bec8e.

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -23,15 +23,6 @@ namespace ConsoleApplication2
             Console.WriteLine("chngeeeeeeeeeeeeeeeeeeeee conflict feature");
 
             Console.WriteLine("git gui");
-
-
-
-            Console.WriteLine("chngeeeeeeeeeeeeeeeeeeeee conflict");
-
-
-            Console.WriteLine("chngeeeeeeeeeeeeeeeeeeeee conflict feature");
-
-            Console.WriteLine("git gui");
         }
     }
 }
